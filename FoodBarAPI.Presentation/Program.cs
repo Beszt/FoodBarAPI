8df71655e7aaf047c513652f917a39a3692f6138
Maciej Obarzanek <maciej.obarzanek@gmail.com>
2023-12-06 21:55:34 +0100

    Add Swagger support to the API

--- FoodBarAPI.Presentation/Program.cs
+++ FoodBarAPI.Presentation/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -16,6 +17,12 @@ await seeder.Seed();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FoodBarAPI");
+});
 
 app.UseRouting();
 app.MapDefaultControllerRoute();
